7570da9a97275bff19c0c59d59bc42bba21facdd
Egondu <ihemebiriegondu@gmail.com>
2024-10-09 00:28:33 +0100

    updated the form field

--- src/components/dateTimeInput.jsx
+++ src/components/dateTimeInput.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function DateTimeInput({ id, type, text }) {
+  const [todayDate, setTodayDate] = useState("");
+
+  useEffect(() => {
+    const date = new Date().toISOString().slice(0, 10);
+    console.log(date);
+    setTodayDate(date);
+  }, []);
+
   return (
     <div className="grow flex flex-col">
-      <label htmlFor="dateInput" className="sm:text-lg text-base sm:mb-2 mb-1 text-slate-700">
+      <label
+        htmlFor="dateInput"
+        className="sm:text-lg text-base sm:mb-2 mb-1 text-slate-700"
+      >
         {text}
       </label>
       <input
         type={type}
         name={id}
         id={id}
+        min={id === "dateInput" ? todayDate : ''}
         className="md:px-3 px-2 md:py-4 py-1 rounded-md outline-none bg-transparent border-2 border-gray-400 text-base"
       />
     </div>
--- src/components/maps.jsx
+++ src/components/maps.jsx
@@ -1,20 +1,49 @@
-import React from "react";
-import { GoogleMap, Marker } from "@react-google-maps/api";
+import React, { useEffect, useState } from "react";
+import { GoogleMap, MarkerF } from "@react-google-maps/api";
 
 const Map = () => {
+  const [defaultCenter, setDefaultCenter] = useState({
+    lat: 6.5244,
+    lng: 3.3792,
+  });
+
   const mapStyles = {
     height: "100%",
     width: "100%",
   };
 
-  const defaultCenter = {
-    lat: 6.5244,
-    lng: 3.3792,
+  const options = {
+    mapTypeControl: false,
+    fullscreenControl: false,
   };
 
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setDefaultCenter({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.log(error.message);
+          setDefaultCenter({ lat: 6.5244, lng: 3.3792 });
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
   return (
-    <GoogleMap mapContainerStyle={mapStyles} zoom={13} center={defaultCenter}>
-      <Marker position={defaultCenter} />
+    <GoogleMap
+      mapContainerStyle={mapStyles}
+      zoom={18}
+      center={defaultCenter}
+      options={options}
+    >
+      <MarkerF position={defaultCenter} />
     </GoogleMap>
   );
 };
